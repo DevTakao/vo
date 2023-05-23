df13968dde7e58edc1c688547d27625cbc7ecb10
TakaoNexstack <takao@nexbe.sg>
2023-05-24 03:22:30 +0630

    fix letter case

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
     <link
       href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=Rancho&display=swap"
       rel="stylesheet"
