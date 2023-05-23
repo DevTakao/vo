543e7359d043e4650bb332a4bb6b5826e90ef362
TakaoNexstack <takao@nexbe.sg>
2023-05-23 17:08:33 +0630

    hero image added

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,9 +1,22 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import HeroImage from "assets/15740.png";
 
 const HeroSection = () => {
-  return <div css={styles.HeroSection}>HeroSection</div>;
+  return (
+    <div css={styles.HeroSection}>
+      <img css={styles.image} src={HeroImage} alt="Person using laptop" />
+    </div>
+  );
 };
 
 export default HeroSection;
-const styles = {};
+const styles = {
+  image: css`
+    filter: brightness(80%);
+    width: 100%;
+    height: calc(100vh - 50px);
+    object-fit: cover;
+    object-position: top left;
+  `,
+};
