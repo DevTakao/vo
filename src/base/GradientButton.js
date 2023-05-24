1b3cd756debb1e9dd51e2ae21f3c3f48ab1c2f60
TakaoNexstack <takao@nexbe.sg>
2023-05-24 13:35:56 +0630

    hover effect

--- src/base/GradientButton.js
+++ src/base/GradientButton.js
@@ -14,14 +14,17 @@ const styles = {
   GradientButton: css`
     width: 280px;
     height: 74px;
-    background: transparent linear-gradient(270deg, #3ae7ab 0%, #2dc4ea 100%) 0%
-      0% no-repeat padding-box;
+    background: linear-gradient(270deg, #3ae7ab 0%, #2dc4ea 100%);
     border-radius: 50px;
     border-width: 0;
     text-align: center;
     font: normal normal bold 20px/27px Open Sans;
     letter-spacing: 0px;
     color: #ffffff;
-    cursor: pointer;
+
+    :hover {
+      cursor: pointer;
+      background: linear-gradient(90deg, #3ae7ab 0%, #2dc4ea 100%);
+    }
   `,
 };
