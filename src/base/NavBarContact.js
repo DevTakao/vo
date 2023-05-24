b13a64d61a26a43c037b9f36778656823aed535d
TakaoNexstack <takao@nexbe.sg>
2023-05-24 13:31:57 +0630

    nav bar details

--- src/base/NavBarContact.js
+++ src/base/NavBarContact.js
@@ -1,8 +1,41 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import { FaPhoneAlt, FaWhatsapp, FaEnvelope } from "react-icons/fa";
+import { HiDocumentText } from "react-icons/hi";
+import { BsFillCaretDownFill } from "react-icons/bs";
 
 const NavBarContact = () => {
-  return <div css={styles.NavBarContact}>NavBarContact</div>;
+  return (
+    <nav css={styles.NavBarContact}>
+      <div className="navbar-spacer-left"></div>
+      <div className="navbar-button">
+        <FaPhoneAlt size={16} css={styles.icon} />
+        <div>6248 0838</div>
+      </div>
+      <div className="navbar-button">
+        <FaWhatsapp size={16} css={styles.icon} />
+        <div>8484 9948</div>
+      </div>
+      <div className="navbar-button">
+        <FaEnvelope size={16} css={styles.icon} />
+        <div>info@visibleone.com</div>
+      </div>
+      <div className="navbar-button actions">
+        <div css={styles.quoteBtn}>
+          <HiDocumentText size={18} />
+          QUOTE
+        </div>
+        <div css={styles.localeBtn}>
+          SG EN
+          <BsFillCaretDownFill size={18} />
+        </div>
+      </div>
+      {/* <div className="navbar-button">
+        
+      </div> */}
+      <div className="navbar-spacer-right"></div>
+    </nav>
+  );
 };
 
 export default NavBarContact;
@@ -10,5 +43,64 @@ const styles = {
   NavBarContact: css`
     height: 40px;
     background-color: #eee;
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+    .navbar-button {
+      flex: 1 1;
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      gap: 10px;
+    }
+    .navbar-spacer-left {
+      flex: 7 1;
+    }
+    .navbar-spacer-right {
+      width: 250px;
+      flex-shrink: 2;
+    }
+    .actions {
+      margin-left: 20px;
+      gap: 20px;
+    }
+  `,
+
+  icon: css`
+    color: #2dc4ea;
+  `,
+
+  quoteBtn: css`
+    background-color: #37b34a;
+    color: #fff;
+    padding: 3px 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+    border-radius: 3px;
+
+    :hover {
+      cursor: pointer;
+      background-color: #37c34a;
+    }
+  `,
+  localeBtn: css`
+    border: 1px solid #2dc4ea;
+    color: #2dc4ea;
+    padding: 3px 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+    border-radius: 3px;
+    white-space: nowrap;
+
+    :hover {
+      cursor: pointer;
+      background-color: #2dc4ea;
+      color: #fff;
+    }
   `,
 };
--- src/base/NavBarRoutes.js
+++ src/base/NavBarRoutes.js
@@ -1,15 +1,73 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import { FaAngleDown, FaSearch } from "react-icons/fa";
 
 const NavBarRoutes = () => {
-  return <div css={styles.NavBarRoutes}>NavBarRoutes</div>;
+  return (
+    <nav css={styles.NavBarRoutes}>
+      <div className="navbar-spacer-left"></div>
+      <div className="navbar-button no-icon">Home</div>
+      <div className="navbar-button">
+        Services
+        <FaAngleDown size={12} />
+      </div>
+      <div className="navbar-button">
+        Company
+        <FaAngleDown size={12} />
+      </div>
+      <div className="navbar-button no-icon">Blog</div>
+      <div className="navbar-button">
+        Contact Us
+        <FaAngleDown size={12} />
+      </div>
+      <FaSearch size={18} className="navbar-button search-icon" />
+      <div className="navbar-spacer-right"></div>
+    </nav>
+  );
 };
 
 export default NavBarRoutes;
 const styles = {
   NavBarRoutes: css`
+    font-size: 14px;
     height: 40px;
-    background-color: #fff;
-    box-shadow: -1px 0px 3px 1px #ababab;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+
+    .navbar-button {
+      margin-right: 25px;
+      white-space: nowrap;
+      flex: 1 1;
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      gap: 5px;
+
+      :hover {
+        cursor: pointer;
+        color: #2dc4ea;
+      }
+    }
+    .navbar-spacer-left {
+      flex: 20 1;
+    }
+    .navbar-spacer-right {
+      width: 250px;
+      height: 100%;
+      flex-shrink: 2;
+    }
+    .no-icon {
+      flex: 0;
+    }
+    .search-icon {
+      margin-left: 75px;
+      margin-right: 0;
+      justify-content: flex-end;
+      :hover {
+        color: #2dc4ea;
+        cursor: pointer;
+      }
+    }
   `,
 };
